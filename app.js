10e7cd9217994cf555eb99516a99d5f31f0fe85b
Jaypax Ginete <killer.tilapia@gmail.com>
2015-09-09 11:26:06 +0800

    base code

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var x = 1;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/html/index.html');
+});
+
+io.on('connection', function(socket){
+    // do nothing
+});
+
+setInterval(function(){
+  console.log('Emiting new value');
+
+  io.emit('new val', {
+    value_x: x += 1,
+    value_y: random(0,10)
+  });
+
+}, 1500);
+
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+
+
+// utils
+function random(low, high){
+  return Math.random() * (high - low) + low;
+}
---
+++ package.js
@@ -0,0 +1,6 @@
+{
+  "name":"Arduino-Python SFD 2015 demo - Consumer side",
+  "version": "0.0.1",
+  "description": "Consumer side demo for Arduino-Python",
+  "dependencies":{}
+}
